bfe3050ef37153af6a6c4fa384d1a70d6d383637
ranckprogram <1474574022@qq.com>
2022-03-02 14:38:17 +0800

    antdv Spin 拓展静态方法

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Spin from "./decorator/Spin.js"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  mounted() {
+    console.log(Spin)
+    Spin.show()
+
+    window.loading = Spin
   }
 }
 </script>
